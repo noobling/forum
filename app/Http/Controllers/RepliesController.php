013af97ebb050cecb5b43e08a445253a831176c8
0x0 <ghstoreinvesting@gmail.com>
2018-01-07 20:02:33 +0800

    Add comments

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -7,11 +7,20 @@ use App\Thread;
 
 class RepliesController extends Controller
 {
+    /**
+     * Create a new RepliesController instance
+     */
     public function __construct()
     {
         $this->middleware('auth');
     }
-    
+
+    /**
+     * Persist a new reply
+     * 
+     * @param Thread $thread
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Thread $thread)
     {
         $thread->addReply([
--- app/Reply.php
+++ app/Reply.php
@@ -5,9 +5,17 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
-{
+{   
+    /**
+     * Don't apply mass assignment protection
+     */
     protected $guarded = [];
 
+    /**
+     * A reply belongs to an owner
+     * 
+     * return \Illumintate\Database\Eloquent\Relations\BelongsTo
+     */
     public function owner()
     {
         // Have to explicit with the foreign key id since we are not using user but owner
