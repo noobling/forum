b51366e2cef8bb83044f4b71a22e8268fc3051ca
0x0 <ghstoreinvesting@gmail.com>
2018-01-07 20:31:34 +0800

    Add navbar link to threads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,12 +31,13 @@
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
+
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li><a href="/threads">Threads</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
