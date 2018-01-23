353fa8336beb3c748dac353f0968fed2d9638da9
0x0 <ghstoreinvesting@gmail.com>
2018-01-23 22:15:54 +0800

    Remove unneeded panel to make it look better

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,31 +4,26 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Threads</div>
-                    <div class="panel-body">
-                        @foreach ($threads as $thread)
-                            <div class="panel panel-default">
 
-                                <div class="panel-heading">
-                                    <div class="level">
-                                        <h4 class="flex">
-                                            <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                                        </h4>
-                                        <strong>
-                                            <a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</a>
-                                        </strong>
-                                    </div>
-                                </div>
+                @foreach ($threads as $thread)
+                    <div class="panel panel-default">
 
-                                <div class="panel-body">
-                                    <p>{{ $thread->body }}</p>
-                                </div>
+                        <div class="panel-heading">
+                            <div class="level">
+                                <h4 class="flex">
+                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+                                </h4>
+                                <strong>
+                                    <a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</a>
+                                </strong>
                             </div>
-                        @endforeach
+                        </div>
+
+                        <div class="panel-body">
+                            <p>{{ $thread->body }}</p>
+                        </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
-    </div>
 @endsection
