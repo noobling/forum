70663ca87e8f2c2bf50632eed1c495cc746a87cc
0x0 <ghstoreinvesting@gmail.com>
2018-02-25 14:39:03 +0800

    Refactor notifications test

--- tests/Feature/NotificationsTest.php
+++ tests/Feature/NotificationsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Notifications\DatabaseNotification;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -10,72 +11,51 @@ class NotificationsTest extends TestCase
 {
     use RefreshDatabase;
 
+    function setup()
+    {
+        parent::setUp();
+
+        $this->signIn();
+    }
+
     /** @test */
     public function a_notification_is_triggered_when_a_different_user_adds_reply_to_thread()
     {
-        $this->signIn();
-
         $thread = create('App\Thread')->subscribe();
-
         $this->assertCount(0, auth()->user()->notifications);
-
         $thread->addReply([
             'user_id' => auth()->id(),
             'body' => 'good'
         ]);
-
         $this->assertCount(0, auth()->user()->fresh()->notifications);
-
         $thread->addReply([
             'user_id' => create('App\User')->id,
             'body' => 'good body'
         ]);
-
         $this->assertCount(1, auth()->user()->fresh()->notifications);
     }
 
     /** @test */
     public function a_user_can_view_unread_notifications()
     {
-        $this->signIn();
-
-        $thread = create('App\Thread')->subscribe();
-
-        $user = auth()->user();
-
-        $this->assertCount(0, $user->notifications);
-
-        $thread->addReply([
-            'user_id' => create('App\User')->id,
-            'body' => 'good body'
-        ]);
-
-        $response = $this->getJson("/profiles/{$user->name}/notifications")->json();
-        $this->assertCount(1, $response);
+        create(DatabaseNotification::class);
+        $this->assertCount(
+            1,
+            $this->getJson("/profiles/" . auth()->user()->name . "/notifications")->json()
+        );
     }
 
     /** @test */
     public function a_user_can_clear_notifications()
     {
-        $this->signIn();
+        create(DatabaseNotification::class);
 
-        $thread = create('App\Thread')->subscribe();
+        tap(auth()->user(), function ($user) {
+            $this->assertCount(1, $user->fresh()->unreadNotifications);
 
-        $user = auth()->user();
+            $this->delete("/profiles/{$user->name}/notifications/" . $user->unreadNotifications->first()->id);
 
-        $this->assertCount(0, $user->notifications);
-
-        $thread->addReply([
-            'user_id' => create('App\User')->id,
-            'body' => 'good body'
-        ]);
-
-        $this->assertCount(1, $user->fresh()->unreadNotifications);
-
-        $notificationId = $user->unreadNotifications->first()->id;
-
-        $this->delete("/profiles/{$user->name}/notifications/{$notificationId}");
-
-        $this->assertCount(0, $user->fresh()->unreadNotifications);
+            $this->assertCount(0, $user->fresh()->unreadNotifications);
+        });
     }
 }
